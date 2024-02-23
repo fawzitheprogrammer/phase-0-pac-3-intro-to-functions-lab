d09a277a724cdca7c5501c9f8116dd5b73667675
Fawzi Gharib <fawz11gharib@gmail.com>
2024-02-23 23:51:22 +0300

    sayHiToHeadphonedRoommate function

--- index.js
+++ index.js
@@ -17,3 +17,14 @@ function logShout(string) {
 function logWhisper(string) {
     console.log(string.toLowerCase());
 }
+
+// 5) sayHiToHeadphonedRoommate function
+function sayHiToHeadphonedRoommate(string) {
+    if (string === string.toLowerCase()) {
+        return "I can't hear you!";
+    } else if (string === string.toUpperCase()) {
+        return "YES INDEED!";
+    } else if (string === "Let's have dinner together!") {
+        return "I would love to!";
+    }
+}
